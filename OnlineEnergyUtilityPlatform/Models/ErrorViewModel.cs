7e252db8fa7ab5fa128caeac3339c4965685cb18
rares <rares@LAPTOP-JHOIHMPK>
2023-02-04 01:40:48 +0200

    Add project files.

---
+++ OnlineEnergyUtilityPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineEnergyUtilityPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
