818dd13b9772ca3401446abdc280e23bbb5e4ae3
raresparlea48 <raresparlea48@yahoo.com>
2023-02-08 00:18:56 +0200

    adaugare pagini pentru device

---
+++ OnlineEnergyUtilityPlatform/Views/Client/Devices.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineEnergyUtilityPlatform.Views.Client
+{
+    public class DevicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
