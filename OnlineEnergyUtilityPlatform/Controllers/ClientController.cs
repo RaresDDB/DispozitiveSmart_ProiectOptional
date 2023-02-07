38c3387e224345179281762ac1b34c99ef89fd45
raresparlea48 <raresparlea48@yahoo.com>
2023-02-08 00:12:23 +0200

    modificare client controller

--- OnlineEnergyUtilityPlatform/Controllers/ClientController.cs
+++ OnlineEnergyUtilityPlatform/Controllers/ClientController.cs
@@ -8,16 +8,25 @@ namespace OnlineEnergyUtilityPlatform.Controllers
     
     public class ClientController : Controller
     {
-
-        public ClientController()
+        private readonly IDeviceService _deviceService;
+        public ClientController(IDeviceService deviceService)
         {
- 
+            _deviceService = deviceService;
         }
 
         [Authorize(Roles = "client")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize(Roles = "client")]
+        public IActionResult Devices()
+        {
+            var userId = HttpContext.Request.Cookies["userId"];
+            var userDevices = _deviceService.GetAllDevicesByUserId(userId);
+
+            return View(new GetDevicesViewModel { Devices = userDevices.Result.Devices });
         }
     }
 }
