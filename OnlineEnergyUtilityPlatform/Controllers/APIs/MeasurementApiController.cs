3c05558a131c58ef54065db5aae0d7f4364a2a6f
raresparlea48 <raresparlea48@yahoo.com>
2023-02-08 21:16:35 +0200

    adaugare consum api

---
+++ OnlineEnergyUtilityPlatform/Controllers/APIs/MeasurementApiController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEnergyUtilityPlatform.DTOs.DeviceMeasurement;
+using OnlineEnergyUtilityPlatform.DTOs.Measurement;
+using OnlineEnergyUtilityPlatform.Exceptions;
+using OnlineEnergyUtilityPlatform.Services.Interfaces;
+
+namespace OnlineEnergyUtilityPlatform.Controllers.APIs
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class MeasurementApiController : ControllerBase
+    {
+        private readonly IMeasurementService _measurementService;
+
+        public MeasurementApiController(IMeasurementService measurementService)
+        {
+            _measurementService = measurementService;
+        }
+
+        [HttpGet("GetAllMeasurement")]
+        public IActionResult GetAllMeasurement()
+        {
+            return Ok(_measurementService.GetAllMeasurement());
+        }
+
+        [HttpPost("AddMeasurement")]
+        public IActionResult AddMeasurement([FromBody] AddMeasurementDTO measurement)
+        {
+            try
+            {
+                _measurementService.AddMeasurement(measurement);
+                return Ok(measurement);
+            }
+            catch(MeasurementException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("GetMeasurementById")]
+        public IActionResult GetMeasurementById(int id)
+        {
+            try
+            {
+                var measurement = _measurementService.GetMeasurementById(id);
+                return Ok(measurement);
+            }
+            catch (MeasurementException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("UpdateMeasurement")]
+        public IActionResult UpdateMeasurement([FromBody] UpdateMeasurementDTO measurement)
+        {
+            try
+            {
+                _measurementService.UpdateMeasurement(measurement);
+                return Ok(measurement);
+            }
+            catch (MeasurementException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("DeleteMeasurement")]
+        public IActionResult DeleteMeasurement([FromBody] DeleteMeasurementDTO measurement)
+        {
+            try
+            {
+                _measurementService.DeleteMeasurement(measurement);
+                return Ok();
+            }
+            catch (MeasurementException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("GraphicMeasurements")]
+        public IActionResult GetAllMeasurementByDayByDevice(int deviceId, DateTime timeStamp)
+        {
+            try
+            {
+                var measuerements = _measurementService.GetAllMeasurementByDayByDevice(deviceId, timeStamp);
+                return Ok(measuerements);
+            }
+            catch (MeasurementException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
