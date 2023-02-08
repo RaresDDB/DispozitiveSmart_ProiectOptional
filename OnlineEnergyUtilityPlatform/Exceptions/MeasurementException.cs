1dde602effd06e2edf833c1464b9f69caaa4fd9f
raresparlea48 <raresparlea48@yahoo.com>
2023-02-08 21:07:35 +0200

    adaugare exceptie consum

---
+++ OnlineEnergyUtilityPlatform/Exceptions/MeasurementException.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineEnergyUtilityPlatform.Exceptions
+{
+    public class MeasurementException : Exception
+    {
+        public MeasurementException()
+        {
+
+        }
+
+        public MeasurementException(string? message) : base(message)
+        {
+
+        }
+    }
+}
