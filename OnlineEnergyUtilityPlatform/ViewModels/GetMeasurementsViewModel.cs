422162fc229ccf98b70b90ba0cedca1cccce390a
raresparlea48 <raresparlea48@yahoo.com>
2023-02-08 21:14:35 +0200

    adugare viewmodel pt consum

---
+++ OnlineEnergyUtilityPlatform/ViewModels/GetMeasurementsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using OnlineEnergyUtilityPlatform.DTOs.Device;
+
+namespace OnlineEnergyUtilityPlatform.ViewModels
+{
+    public class GetMeasurementsViewModel
+    {
+        public GetDeviceDTO Device { get; set; }
+    }
+}
